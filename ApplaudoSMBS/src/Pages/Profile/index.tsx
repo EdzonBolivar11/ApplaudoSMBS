22c268b578a25ecc186a4b7547adab34fdd546b1
Edzon Bolivar <edzonbolivar@gmail.com>
2021-02-19 14:33:06 -0600

    Configuración de navegación

---
+++ ApplaudoSMBS/src/Pages/Profile/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View, StyleSheet, Text } from 'react-native';
+
+const index = () => {
+return <View><Text>Profile</Text></View>
+}
+
+const styles = StyleSheet.create({});
+
+index.defaultProps = {};
+
+index.propTypes = {};
+
+export default index;
---
+++ ApplaudoSMBS/src/utils/Types/index.ts
@@ -0,0 +1,5 @@
+//Navigation
+export type RootStack = {
+    Profile: undefined;
+    Home: undefined;
+}
